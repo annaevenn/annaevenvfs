cbab0ca6fc8369b0ea4a4f2ee2887958324cb912
annaevenn <anna.camila.cortez@gmail.com>
2026-07-07 15:33:16 -0600

    timer

---
+++ Assets/scripts/UI/Timer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+    }
+}
